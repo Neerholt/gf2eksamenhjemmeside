02fc91effb48c3274fa95e5c48f3ddf436ac9b04
Neerholt <50305323+Neerholt@users.noreply.github.com>
2019-10-03 13:33:50 +0200

    Create uploadetopskrift.php

---
+++ uploadetopskrift.php
@@ -0,0 +1,341 @@
+<?php 
+
+if(isset($_POST['GEM'])){
+    
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+$databasename = 'logineksamen';
+
+$name = $_POST ['navn'];
+$efternavn = $_POST ['efternavn'];
+$besked = $_POST ['besked'];
+ 
+    
+
+$connect = mysqli_connect($servername, $username, $password, $databasename);
+
+if(!$connect){
+    die("Connectiuon failed because" .mysqli_connect_error());
+} 
+        
+$sql_tabel = "INSERT INTO `lavopskrift`(`ID`, `Antal`, `Madret`, `Besked`) VALUES (NULL,'$name','$efternavn','$besked')";
+
+$data = mysqli_query($connect,$sql_tabel);
+
+
+mysqli_close($connect);
+
+}
+?>    
+<?php
+session_start();
+if(!isset($_SESSION['login'])){
+    header("location:index.php");
+    exit;
+}
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Kogebogen.com</title>
+    </head>
+    <body>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"><!-- CSS Style link til søge funktion-->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"><!--CSS Style link til mine social media-->
+<!--Hele hjemmesidens layout start-->       
+  <style>
+    body{
+        background-color: #EEE;
+    }
+   
+    #header{
+        background-color: #999999;
+        color: black;
+        padding: 1px;
+        text-align: center;
+    }  
+    
+    #container{
+        background-color: white;
+        width: 1100px;
+        margin-left: auto;
+        margin-right: auto;
+    }
+    
+    #content{
+        padding: 10px;  
+    }
+    
+    #nav{
+        width: 252px;
+        float: left;
+        text-align: center;
+        border: 2px solid red;
+    }
+    
+    #nav ul{
+        list-style-type: none;
+        padding: 0px;
+    }
+    
+    #main{
+        width: 844px;
+        float: right;
+    }
+    
+    #textkasse{
+        background-color: #FFFFFF;
+        width: 730px;
+        border: 3px solid grey ;
+        padding: 10px 3px 3px 3px;
+        margin: 30px;
+}
+    
+    #footer{
+        clear: both;
+        padding: 3px;
+        text-align: right;
+    }
+
+</style>
+
+<!--The start of my style script to make the sign-in form look nice -->
+ <style>
+.formcss {
+  background-color: #FFFFFF;
+  border: none;
+  color: black;
+  padding: 7px 31px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+  margin-top: 10px;
+}
+
+.formcss {background-color: #EEE;} 
+</style>
+<!--The ending of my style script to make the sign-in form look nice -->
+
+<!--The start of my css style to my button-->
+ <style> 
+.logincss {
+  background-color: #f44336; 
+  border: none;
+  color: white;
+  padding: 7px 31px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+}
+</style>   
+ <!--The ending of my css style to my button-->
+ 
+ <!--The start of my css style to my menu buttons-->
+ <style>
+.buttonmenu {
+  background-color: #f44336;
+  border: none;
+  color: white;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 2px 4px;
+  cursor: pointer;
+}
+
+</style><!--The ending of my css style to my menu buttons-->
+
+<!--The start of my "main menu container"-->
+<style>
+div.ex1 {
+  background-color: white;
+  width: 845px;
+  height: 830px;
+  overflow: auto;
+}
+</style>
+<!--The end of my "main menu container"-->
+
+<!--The start of my css nav style-->
+<style>
+body {
+  margin: 0;
+}
+
+li a {
+  display: block;
+  color: #000;
+  padding: 8px 16px;
+  text-decoration: none;
+}
+
+li a.active {
+  background-color: #f44336;
+  color: white;
+}
+
+li a:hover:not(.active) {
+  background-color: #EEE;
+  color: white;
+}
+</style>
+    
+<!--The start of my css style to the searchbar-->
+<style>
+
+.topnav .search-container {
+  float: left;
+}
+
+.topnav input[type=text] {
+  padding: 6px;
+  margin-top: 10px;
+  margin-left: 36px;
+  font-size: 17px;
+  border: none;
+  background: #EEE;
+}
+
+.topnav .search-container button {
+  float: right;
+  padding: 6px 10px;
+  margin-top: 10px;
+  margin-right: 20px;
+  background: #f44336;
+  font-size: 17px;
+  border: none;
+  cursor: pointer;
+}
+.topnav .search-container button:hover {
+  background: #ccc;
+}
+
+</style><!--The end of my css style to the searchbar-->
+
+<!--The start of my css style to my social media buttons-->
+<style>
+.fa {
+  padding: 20px;
+  font-size: 27px;
+  width: 30px;
+  text-align: center;
+  text-decoration: none;
+  margin: 5px 2px;
+  border-radius: 50%;
+}
+
+.fa:hover {
+    opacity: 0.7;
+}
+
+.fa-facebook {
+  background: #3B5998;
+  color: white;
+}
+
+.fa-twitter {
+  background: #55ACEE;
+  color: white;
+}
+
+.fa-instagram {
+  background: #125688;
+  color: white;
+}
+
+.fa-pinterest {
+  background: #cb2027;
+  color: white;
+}
+
+.fa-tumblr {
+  background: #2c4762;
+  color: white;
+}
+
+</style><!--The end of my css style to my social media buttons-->
+
+<!--Start:Det her er style til mine textfelter-->
+<style>
+.idk {
+  background-color: #EEE; 
+  border: none;
+  color: black;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+}
+
+
+.idk {background-color: #EEE;} 
+
+</style>
+<!--Slut:Det her er style til mine textfelter-->
+
+
+               
+<!--The end of my css nav style-->
+
+ <!--Hele hjemmesidens layout slut-->     
+        <div id="container"><!--The start of my div "container"-->
+            <div id="header"><!--The start of my div "header" -->
+                <h1><strong>Kogebogen.com</strong></h1>
+            </div><!--The ending of my div "header"-->
+             <div id="nav"><!--The start of my div "nav"-->
+                <center><h2>Admin panel</h2></center>
+                
+                <form method="POST" action="opskriftindsendt.php">
+                <input class="logincss" type="submit" name="logut" value="Se indsendt opskrifter">
+               </form>
+                
+                <hr>
+                
+                <form method="POST" action="index.php">
+                <input class="logincss" type="submit" name="logut" value="Cancel post">
+                </form>
+                
+                <hr>
+                  
+                <form method="POST" action="">
+                <input class="logincss" type="submit" name="logut" value="Nulstil clade">
+                  </form>
+               
+                <hr>
+                
+                <form method="POST" action="index.php">
+                <input class="logincss" type="submit" name="logut" value="Log ud">
+                </form>
+
+           </div><!--The ending of my div "nav"-->
+           <div id="main"><!--The start of my div "main"-->   
+                <div id="main"><!--Div start main-->
+                    <div class="ex1"><!--Div start More recipes -->
+                        <div id="information"><!--Starten på mit div hvor en bruger kan give mig deres navn, efternavn og en besked-->
+                            
+                            <center><h1>Opskrift er bliver uploadet til jers hjemmeside.</h1></center>  
+                        </div><!--slutning på mit div hvor en bruger kan give mig deres navn, efternavn og en besked-->
+                    </div> <!--Div slut More recipes -->  
+           </div><!--The ending of my div "main"-->
+           <div id="footer">
+               <hr>
+             Victor Neerholt & Frederik Vilhelmsen &copy; <?php echo date("Y"); ?> 
+           </div>
+        </div><!--The ending of my div "container"-->
+        
+    </body>
+</html>
+
